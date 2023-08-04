995d8c9ce7c824fc94ede9844f4eea573f8cae52
Nilay Savant <nilayofficial@gmail.com>
2023-08-04 19:04:02 +0530

    Fix hyrarchy for sensor coll

--- src/systems/physics.rs
+++ src/systems/physics.rs
@@ -1,23 +1,24 @@
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 
-use crate::components::{ActiveGround, Ground, GroundSensor, RollingBall};
+use crate::components::{ActiveGround, BelongsToGround, Ground, GroundSensor, RollingBall};
 
+#[allow(clippy::type_complexity)]
 pub fn ball_touching_ground(
     mut commands: Commands,
-    query_ball: Query<Entity, (With<RollingBall>, With<Collider>)>,
-    query_ground: Query<Entity, (With<GroundSensor>, With<Collider>)>,
+    balls: Query<Entity, (With<RollingBall>, With<Collider>)>,
+    ground_sensors: Query<(Entity, &BelongsToGround), (With<GroundSensor>, With<Collider>)>,
     rapier_context: Res<RapierContext>,
 ) {
-    let Ok(ball_ent) = query_ball.get_single() else { return; };
+    let Ok(ball_ent) = balls.get_single() else { return; };
 
-    for ground_ent in query_ground.iter() {
-        let Some(is_intersecting) = rapier_context.intersection_pair(ball_ent, ground_ent, ) else { continue; };
+    for (sensor_ent, BelongsToGround(ground_ent)) in ground_sensors.iter() {
+        let Some(is_intersecting) = rapier_context.intersection_pair(ball_ent, sensor_ent ) else { continue; };
         // if intersecting set ground as active else inactive...
         if is_intersecting {
-            commands.entity(ground_ent).insert(ActiveGround);
+            commands.entity(*ground_ent).insert(ActiveGround);
         } else {
-            commands.entity(ground_ent).remove::<ActiveGround>();
+            commands.entity(*ground_ent).remove::<ActiveGround>();
         }
     }
 }
