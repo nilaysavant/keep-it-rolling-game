1a20886cab8bcc209047641d245f54a2ac6ad3c1
Nilay Savant <nilayofficial@gmail.com>
2023-08-06 00:05:55 +0530

    Add game title const and use it

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,5 @@
+pub const GAME_TITLE: &str = "Keep It Rollin";
+
 pub const GROUND_ANGLE: f32 = std::f32::consts::FRAC_PI_8 / 2.;
 pub const GROUND_LENGTH: f32 = 10.0;
 pub const GROUND_WIDTH: f32 = 10.0;
--- src/systems/window.rs
+++ src/systems/window.rs
@@ -1,9 +1,11 @@
 use bevy::prelude::*;
 
+use crate::constants::GAME_TITLE;
+
 /// # Setup Window
 ///
 /// System updates and sets up the window attributes
 pub fn setup_window(mut windows: Query<&mut Window>) {
     let mut window = windows.get_single_mut().unwrap();
-    window.title = "Keep It Rolling".to_string();
+    window.title = GAME_TITLE.to_string();
 }
